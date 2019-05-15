229389e6ebd293d98d353efb15d4e502fb3a027e
Khaled Osman <khaled.osmaan@gmail.com>
2019-05-15 14:47:16 +0200

    fix subscriptions

--- server.js
+++ server.js
@@ -1,40 +1,32 @@
 import dotenv from 'dotenv'
 import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
+import { ApolloServer, graphiqlExpress } from 'apollo-server-express'
 import compression from 'compression'
 import { schema } from './data/schema'
 import { db } from './connectors'
+import http from 'http'
 dotenv.config()
-// require('dotenv').config()
 const ENGINE_API_KEY = process.env.ENGINE_API_KEY
 
 const app = express()
 app.use(compression())
-
+// app.use(
+//   '/graphiql',
+//   graphiqlExpress({
+//     endpointURL: '/graphql'
+//   })
+// )
 const server = new ApolloServer({
   schema,
   tracing: true,
   cacheControl: true,
   engine: {
     apiKey: ENGINE_API_KEY
-  },
-  subscriptions: {
-    // onConnect: (connectionParams, webSocket) => {
-    //   if (connectionParams.authToken) {
-    //     return validateToken(connectionParams.authToken)
-    //       .then(findUser(connectionParams.authToken))
-    //       .then(user => {
-    //         return {
-    //           currentUser: user
-    //         }
-    //       })
-    //   }
-
-    //   throw new Error('Missing auth token!')
-    // }
   }
 })
 server.applyMiddleware({ app })
-app.listen({ port: 4000 }, () =>
+const httpServer = http.createServer(app)
+server.installSubscriptionHandlers(httpServer)
+httpServer.listen({ port: 4000 }, () => {
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-)
+})
